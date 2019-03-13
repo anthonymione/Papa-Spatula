29c0751d5a86d114451b08d90ed7f15729e853e2
zachwooding <zachwooding@gmail.com>
2019-03-13 11:01:48 -0600

    changed index.html to index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<html lang="en">
+		<html class="background">
+
+		<title>Papa-Spatula</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+		<link href="StyleSheet.css" rel="stylesheet" type="text/css">
+
+	</head>
+
+	<body>
+		<?php include("nav.html");?>
+		<div class="background">
+			<h1 class="group-name">This is Papa Spatula</h1>
+			<div class="about-group-style">
+				<h2 class="about-group">We are your Father Kitchen Appliance. <br><br><br>Your patty flippers, panckae tossers, and your plastic melters. We are creating websites, web apllications and mobile applications for our GIMM 300 (Gaming, Interactive Media, and Mobile Technology) class at Boise State University while demonstrating the use of agile code methodologies!
+				</h2>
+			</div>
+		</div>
+	</body>
+			
+		
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	
+	<div id="bee"> 
+  		<img src="Images/SpatulaJetPack.png"/>
+	</div>
+	
+	
+	
+<script src="mouse-follow.js"></script>
+	
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
+</html>
